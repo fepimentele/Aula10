3280faebfe1797edbf502a82aebe02ff60f6e21f
fepimentele <fernanda.pimentel@aluno.senac.rio>
2026-05-05 19:38:39 -0300

    Inicializar repositório

--- exemplo04.py
+++ exemplo04.py
@@ -3,9 +3,8 @@ print('=== Cálculo de Produtividade ===')
 try: 
     total_produzido = float(input('Valor total da venda: '))
     funcionarios = int(input('total de Funcinários: '))
-
     media_por_funcionario = total_produzido / funcionarios
-    print(f'Média por funcionário: {media_por_funcionario:.2f}')
+    
 #except (ValueError, TypeError):
 except ValueError:
     print('Informe um número.')
